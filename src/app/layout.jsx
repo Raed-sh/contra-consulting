9feff01d593e4268df88a4a58662b3b921eb1505
raed-sh <raedsh.work@gmail.com>
2024-05-27 14:48:15 +0300

    add opengraph image

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,7 +26,7 @@ export const metadata = {
     default:
       'Contra Consulting',
   },
-  description:"HR and consulting compnay in MENA region"
+  description:"HR and consulting compnay in MENA region",
 }
 
 export default function RootLayout({ children }) {
